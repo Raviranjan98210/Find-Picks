40e8a81d17e39e2ce120763cb3679a2ef871befc
Ravi Ranjan Kumar <raviranjan98210@gmail.com>
2020-08-30 00:22:39 +0530

    Find-Pick

---
+++ src/components/SearchBar.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  state = {
+    term: "",
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>Search</label>
+            <input
+              type="text"
+              name="search-image"
+              placeholder="Search Image"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
